65a55e4f8861f7439eda3e8c778fd870ccac4270
Michele Di Salvatore <git@micheledisalvatore.it>
2018-08-01 13:41:54 -0500

    update search input style

--- src/App.styled.js
+++ src/App.styled.js
@@ -3,64 +3,60 @@ import PropTypes from 'prop-types';
 import { Alert, Input } from 'reactstrap';
 
 export const InputsGroup = styled('div')`
-    display: flex;
-    height: calc(1.5em + 1.5rem);
-    justify-content: space-between;
-    position: sticky;
-    top: 0;
-    background-color: white;
-    border-bottom: 2px solid #dee2e6;
+  display: flex;
+  height: calc(1.5em + 0.9rem);
+  justify-content: space-between;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  border-bottom: 2px solid #dee2e6;
 `;
 
 export const Label = styled('label')`
-    padding: .75rem;
-    margin: 0;
-    position: absolute;
-    ${({ right }) => right && 'right: 0;'}
-    ${({ left }) => left && 'left: 0;'}
+  padding: 0.45rem .75rem;
+  margin: 0;
+  ${({ right }) => right && 'right: 0;'}
+  ${({ left }) => left && 'left: 0;'}
 `;
 
-const BaseInput = styled(Input)`
-    align-items: stretch;
-    background-clip: padding-box;
-    border-radius: .25rem;
-    display: inline-block;
-    width: 0;
-    padding: 0;
-    border: none;
+export const SearchInput = styled(Input)`
+  align-items: stretch;
+  background-clip: padding-box;
+  border-radius: .25rem;
+  border: none;
+  display: inline-block;
+  padding: 0;
+  position: absolute;
+  width: 0;
 
-    ${({ visible }) => visible && `
-        border: 1px solid #ced4da;
-        padding: .375rem .75rem;
-        width: 100%;
+  ${({ visible }) => visible && `
+    border: 1px solid #ced4da;
+    padding: .375rem .75rem;
+    width: 100%;
 
-        & ~ label {
-            display: none;
-        }
-    `}
+    & ~ label {
+      display: none;
+    }
+  `}
 `;
 
-BaseInput.propTypes = {
+SearchInput.propTypes = {
   visible: PropTypes.bool.isRequired,
 };
 
-export const SearchInput = BaseInput;
-
-export const AddInput = BaseInput;
-
 export const Dot = styled('div')`
-    height: 25px;
-    width: 25px;
-    background-color: ${({ color }) => color};
-    border-radius: 50%;
-    display: inline-block;
+  height: 25px;
+  width: 25px;
+  background-color: ${({ color }) => color};
+  border-radius: 50%;
+  display: inline-block;
 `;
 
 export const AlertSuccess = styled(Alert).attrs({
   color: 'success',
 })`
-    position: absolute;
-    width: 100%;
-    text-align: center;
-    top: 0;
+  position: absolute;
+  width: 100%;
+  text-align: center;
+  top: 0;
 `;
